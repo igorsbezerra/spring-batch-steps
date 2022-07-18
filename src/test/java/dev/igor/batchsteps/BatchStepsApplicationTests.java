0f3d343c5c272065ea532260c3ed257e393f7a28
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-07-18 19:54:50 -0300

    first commit

---
+++ src/test/java/dev/igor/batchsteps/BatchStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.igor.batchsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchStepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
